115b4a3c738b3f88da717c199bdd2b12fb3c5459
PolskieBadaniaCzytelnictwa <149326679+PolskieBadaniaCzytelnictwa@users.noreply.github.com>
2023-12-19 14:52:16 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,14 +10,14 @@ st.markdown('---')
 image_html = """
 <img src="https://raw.githubusercontent.com/PolskieBadaniaCzytelnictwa/Profile/main/bubble_web.png" alt="Profile czytelnikow" usemap="#imageMap">
 <map name="imageMap">
-    <area shape="rect" coords="250,44,290,146" href="https://www.pbc.pl/badane-tytuly/?filtry=dzienniki-ogolnopolskie" alt="Link 1">
-    <area shape="rect" coords="305,46,345,144" href="https://www.pbc.pl/badane-tytuly/?filtry=dzienniki-regionalne" alt="Link 2">
-    <area shape="rect" coords="360,46,400,144" href="https://www.pbc.pl/badane-tytuly/?filtry=opinii-spoleczno-polityczne,tygodniki" alt="Link 3">
-    <area shape="rect" coords="415,46,455,144" href="https://www.pbc.pl/badane-tytuly/?filtry=telewizyjne" alt="Link 4">
-    <area shape="rect" coords="470,44,510,146" href="https://www.pbc.pl/badane-tytuly/?filtry=kobiece-luksusowe" alt="Link 5">
-    <area shape="rect" coords="525,44,565,146" href="https://www.pbc.pl/badane-tytuly/?filtry=kobiece-poradnikowe" alt="Link 6">
-    <area shape="rect" coords="580,36,620,152" href="https://www.pbc.pl/badane-tytuly/?filtry=poradniczo-rozrywkowe" alt="Link 7">
-    <area shape="rect" coords="690,50,730,138" href="https://www.pbc.pl/badane-tytuly/?filtry=people" alt="Link 8">
+    <area shape="rect" coords="250,44,290,146" href="https://www.pbc.pl/badane-tytuly/?filtry=dzienniki-ogolnopolskie" alt="Link 1" target="_blank">
+    <area shape="rect" coords="305,46,345,144" href="https://www.pbc.pl/badane-tytuly/?filtry=dzienniki-regionalne" alt="Link 2" target="_blank">
+    <area shape="rect" coords="360,46,400,144" href="https://www.pbc.pl/badane-tytuly/?filtry=opinii-spoleczno-polityczne,tygodniki" alt="Link 3" target="_blank">
+    <area shape="rect" coords="415,46,455,144" href="https://www.pbc.pl/badane-tytuly/?filtry=telewizyjne" alt="Link 4" target="_blank">
+    <area shape="rect" coords="470,44,510,146" href="https://www.pbc.pl/badane-tytuly/?filtry=kobiece-luksusowe" alt="Link 5" target="_blank">
+    <area shape="rect" coords="525,44,565,146" href="https://www.pbc.pl/badane-tytuly/?filtry=kobiece-poradnikowe" alt="Link 6" target="_blank">
+    <area shape="rect" coords="580,36,620,152" href="https://www.pbc.pl/badane-tytuly/?filtry=poradniczo-rozrywkowe" alt="Link 7" target="_blank">
+    <area shape="rect" coords="690,50,730,138" href="https://www.pbc.pl/badane-tytuly/?filtry=people" alt="Link 8" target="_blank">
 </map>
 """
 st.markdown(image_html, unsafe_allow_html=True)
